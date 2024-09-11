f91453a492631e3bf93e5b11e4552160dc2ee667
pth-heissend <philipp.hellwig@student.uva.nl>
2024-09-11 16:21:39 +0200

    added rating curves visualization for all game types

--- code/get_lichess_usernames.py
+++ code/get_lichess_usernames.py
@@ -1,8 +1,8 @@
 import re
-with open("../data/lichess_db_standard_rated_2023-10.pgn", "r") as file:
+with open("./data/lichess_db_standard_rated_2016-09.pgn", "r") as file:
     data = file.read()
 users = re.findall(r'White "(\w*)"]', data) + re.findall(r'Black "(\w*)"]', data)
 users = set(users) # include each username only once
 
-with open('../data/usernames.txt', 'w', newline='') as file:
+with open('./data/usernames.txt', 'w', newline='') as file:
     file.write(",".join(users))
