c1638ab7210d465305757f0cb3cb8042f4d4ec0c
pth-heissend <philipp.hellwig@student.uva.nl>
2024-09-12 16:09:59 +0200

    updated rating curves, added usernames txt

--- code/get_lichess_usernames.py
+++ code/get_lichess_usernames.py
@@ -1,8 +1,8 @@
 import re
-with open("./data/lichess_db_standard_rated_2016-09.pgn", "r") as file:
-    data = file.read()
+
+with open("./data/lichess_db_standard_rated_2024-08.pgn", "r") as file:
+    data = file.read(81920000) # limit read to number of max characters
 users = re.findall(r'White "(\w*)"]', data) + re.findall(r'Black "(\w*)"]', data)
 users = set(users) # include each username only once
-
-with open('./data/usernames.txt', 'w', newline='') as file:
+with open('./data/usernames_2024-08.txt', 'w', newline='') as file:
     file.write(",".join(users))
